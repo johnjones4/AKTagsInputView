a4615911b05f150c2a1182ad38c9b6c9274149f4
John Jones <johnjones4@gmail.com>
2017-01-16 12:16:12 -0500

    ui customization

--- Classes/AKTagsDefines.h
+++ Classes/AKTagsDefines.h
@@ -2,5 +2,5 @@
 #define WK_COLOR_GRAY_77 			WK_COLOR(77,77,77,1)
 #define WK_COLOR_GRAY_244			WK_COLOR(244,244,244,1)
 #define TAG_CELL_FONT				AVENIR_NEXT(17)
-#define AVENIR_NEXT(_size) ([UIFont fontWithName:@"Helvetica" size:(_size)])
+#define AVENIR_NEXT(_size) ([UIFont fontWithName:@"Roboto-Bold" size:(_size)])
 #define WK_COLOR_RED_TAG_COLOR 		WK_COLOR(156,39,176,1)
